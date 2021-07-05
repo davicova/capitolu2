950f2e3e92970413c73e5725b2e5f8db16ec0546
davicova <86981409+davicova@users.noreply.github.com>
2021-07-05 15:31:54 -0300

    Add files via upload

---
+++ estruturasderepeticao.py
@@ -0,0 +1,22 @@
+
+print("davi\zua cova dias")
+print("\\novo")
+
+#isso explica a string
+jogo="   minecraft  "
+print(jogo+"!")
+print(jogo.lstrip())
+print(jogo.rstrip()+"!")
+print(jogo.strip()+"!")
+
+#isso explica os numeros
+print(123+3221)
+print(123-236)
+print(32*9)
+print(538/149)
+nome="davi"
+idade=11
+print(nome+" "+str(idade))
+#oi
+
+   
